acdfc64c30cab634a866b81ea38b31f1b3881da1
jinyichengX <146402004+jinyichengX@users.noreply.github.com>
2024-03-04 12:09:25 +0800

    Add files via upload

--- el_kpool_static.h
+++ el_kpool_static.h
@@ -4,20 +4,21 @@
 #include "el_type.h"
 #include "el_klist.h"
 
-/* ¶¨ÒåÄÚºËËùĞèÄÚ´æ³Ø´óĞ¡ */
+/* å®šä¹‰å†…æ ¸æ‰€éœ€é™æ€å†…å­˜æ± å¤§å° */
 #define EL_Pthread_Pendst_Pool_Size 512
 #define EL_Pthread_Suspendst_Pool_Size 256
 
-/* ÒÔN×Ö½Ú¶ÔÆë */
+/* ä»¥Nå­—èŠ‚å¯¹é½ */
 #define KPOOL_BYTE_ALIGNMENT 4
 
-/* ¾²Ì¬ÄÚ´æ³Ø¿ØÖÆÍ· */
+/* é™æ€å†…å­˜æ± æ§åˆ¶å¤´ */
 typedef struct EL_KERNEL_POOL_STATIC_STRUCT
 {
-    LIST_HEAD ObjBlockList; /* ÁĞ±íÒıµ¼Í· */
-    EL_UINT PerBlockSize;   /* ¸ÃÄÚ´æ³ØÖĞ·Ö¿é´óĞ¡ */
-    EL_UINT TotalBlockNum;   /* ¸ÃÄÚ´æ³ØÖĞ×Ü·Ö¿éÊıÄ¿ */
-    EL_UINT UsingBlockCnt;   /* ¸ÃÄÚ´æ³ØÖĞÒÑÊ¹ÓÃ·Ö¿é¼ÆÊı */
+    LIST_HEAD ObjBlockList; /* åˆ—è¡¨å¼•å¯¼å¤´ */
+    EL_UINT PerBlockSize;   /* è¯¥å†…å­˜æ± ä¸­åˆ†å—å¤§å° */
+    EL_UINT TotalBlockNum;   /* è¯¥å†…å­˜æ± ä¸­æ€»åˆ†å—æ•°ç›® */
+    EL_UINT UsingBlockCnt;   /* è¯¥å†…å­˜æ± ä¸­å·²ä½¿ç”¨åˆ†å—è®¡æ•° */
+    LIST_HEAD WaitersToTakePool;/* ç­‰å¾…å†…å­˜æ± çš„çº¿ç¨‹ */
 }EL_KPOOL_INFO_T;
 
 extern EL_RESULT_T EL_stKpoolInitialise(void * PoolSurf,EL_UINT PoolSize,EL_UINT PerBlkSz);
